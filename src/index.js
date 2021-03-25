0021c5d45bc3efdc7f0854971201b2cef04ea87a
yuka830 <66966699+yuka830@users.noreply.github.com>
2021-03-24 22:36:23 -0700

    タスクの完了機能

--- src/index.js
+++ src/index.js
@@ -24,7 +24,9 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    //押された削除ボタンの親ダグ（div）を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
   //divタグの小要素に各要素を設定
